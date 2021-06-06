f77b08438bdb3dfff8eb04f924862b39d85f67ea
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2021-06-05 20:03:25 -0700

    1.3
    profile worked on

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,8 +11,14 @@ export class ProfileComponent implements OnInit {
 
   aboutMe: String;
   projects: String;
+  web = ['/assets/icons/bootstrap.png', '/assets/icons/docker.png', '/assets/icons/node.png'
+        ,'/assets/icons/angular.png', '/assets/icons/react.png', '/assets/icons/vsc.png','/assets/icons/mongodb.png']
   languages = ['/assets/icons/python.png', '/assets/icons/c-plus-plus-logo.png', '/assets/icons/javascript.png'
-              ,'/assets/icons/c-sharp-logo.png', '/assets/icons/java.png', '/assets/icons/css.png','/assets/icons/html.png'];
+              ,'/assets/icons/c-sharp-logo.png', '/assets/icons/java.png', '/assets/icons/css.png','/assets/icons/html.png'
+              ,'/assets/icons/sql.png'];
+  game = ['/assets/icons/unity.png']
+
+
   caught = fetch('/assets/about-me.txt').then(response => response.text()).then(data => {
     this.aboutMe = data;
   })
