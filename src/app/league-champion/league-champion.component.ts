b139ec7f7792e44e4bd8063bd3005656517f12c8
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2021-07-18 11:34:38 -0700

    6.3
    banner file data created

--- src/app/league-champion/league-champion.component.ts
+++ src/app/league-champion/league-champion.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./league-champion.component.css']
 })
 export class LeagueChampionComponent implements OnInit {
+  champion: string;
+  championBanner: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    var href = document.location.href.split('/');
+    this.champion = href[href.length - 1];
+    const firstLetter = this.champion.charAt(0);
+    const upper = firstLetter.toUpperCase();
+
+    this.champion = upper + this.champion.slice(1);
+
+    this.championBanner = "/assets/league_banners/" + this.champion + "_0.jpg";
+    console.log(this.championBanner);
   }
 
 }
