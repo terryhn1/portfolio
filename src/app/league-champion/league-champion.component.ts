83a08309d885c9cf541ee2fd73906274b53681ed
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2021-07-20 16:59:32 -0700

    7.0
    Before any Firebase Realtime Database additions

--- src/app/league-champion/league-champion.component.ts
+++ src/app/league-champion/league-champion.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LeagueChampionComponent implements OnInit {
   champion: string;
   championBanner: string;
+  role: string;
 
   constructor() { }
 
